078fd241cc4a3d3275edfcedc8b66ecbc8237b0e
isaacbrds <isaac.brigido@gmail.com>
2022-06-27 10:17:19 -0300

    Curso de Ruby - Case (Switch)

---
+++ MOD-02/case.rb
@@ -0,0 +1,18 @@
+nome          = ARGV[0]
+senha_passada = ARGV[1]
+
+senha_correta = case nome
+                when 'isaac' then 's1'
+                when 'lucas' then 's2'
+                when 'leo' then 's3'
+                else
+                ''
+                end
+
+autorizado = senha_passada == senha_correta
+
+if autorizado
+  puts 'Autorizado'
+else
+  puts 'Não autorizado'
+end
